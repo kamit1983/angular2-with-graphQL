5143feabb70b902798ae2c200e42584588f80831
Amit Kumar <kamit1983@gmail.com>
2018-02-09 22:15:54 +0530

    Interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,50 @@ import { NgModule } from '@angular/core';
 
 import {GraphQLModule} from './apollo.config';
 import { DoughnutChartComponent, PieChartComponent, BarChartComponent } from 'angular-d3-charts'; // this is needed!
+import { GraphComponent } from './graph/graph.component';
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { GraphComponent } from './graph/graph.component';
+import { RootComponent } from './root/root.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Chart1Component } from './chart1/chart1.component';
+import { Chart2Component } from './chart2/chart2.component';
+import { TableComponent } from './table/table.component';
+import { FormComponent } from './form/form.component';
 
-
+const appRoutes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'tables',      component: TableComponent },
+  { path: 'forms',      component: FormComponent },
+  { path: 'chart1',      component: Chart1Component },
+  { path: 'chart2',      component: Chart2Component },
+  { path: '**', component: DashboardComponent}//PageNotFoundComponent }
+];
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     GraphComponent,
-    DoughnutChartComponent, 
-    PieChartComponent, 
-    BarChartComponent 
+    DoughnutChartComponent,
+    PieChartComponent,
+    BarChartComponent
   ],
   imports: [
     BrowserModule,
-    GraphQLModule
+    RouterModule.forRoot(
+     appRoutes,
+     { enableTracing: true } // <-- debugging purposes only
+   ),
+    GraphQLModule,
+    RootComponent,
+    DashboardComponent,
+    Chart1Component,
+    Chart2Component,
+    TableComponent,
+    FormComponent
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [RootComponent]
 })
 export class AppModule { }
