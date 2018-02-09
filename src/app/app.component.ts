553524e3e0750ba84cc67795821d614a263faf2f
Praveen Nishanth <praveennishanth@gmail.com>
2018-02-09 19:16:31 +0530

    Charts added -need to fix alignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import {GraphQLModule} from './apollo.config';
-
+import { DoughnutChartComponent, PieChartComponent, BarChartComponent } from 'angular-d3-charts'; // this is needed!
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { GraphComponent } from './graph/graph.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    GraphComponent,
+    DoughnutChartComponent, 
+    PieChartComponent, 
+    BarChartComponent 
   ],
   imports: [
     BrowserModule,
